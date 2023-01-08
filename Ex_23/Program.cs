f2d6a39a0587e2f0f660933699e148c06bb1ee9c
Алёна Катанская <alyona.katanskaya@gmail.com>
2023-01-08 13:32:39 +0300

    add folder Ex_23

---
+++ Ex_23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+double result;
+while(i <= N)
+{
+    result = Math.Pow(N, 3);
+    i++;
+
+    Console.WriteLine("Значение числа в кубе равно" + result);
+}
+
+Console.WriteLine("Цикл завершил свою работу");
